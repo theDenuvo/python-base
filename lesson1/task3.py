f3114985efd177894494e5722005a3a63154ec9a
thedenuvo <chelikov.n@yandex.ru>
2021-01-18 16:10:51 +0300

    Решил задачу 3

--- lesson1/task3.py
+++ lesson1/task3.py
@@ -1,15 +1,3 @@
 n = int(input())
 total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
 print("Сумма чисел n + nn + nnn - %d" % total)
-#task 4
-n = abs(int(input()))
-max = n % 10
-while n >= 1:
-    n = n // 10
-    if n % 10 > max:
-        max = n % 10
-    if n > 9:
-        continue
-    else:
-        print(max)
-        break
