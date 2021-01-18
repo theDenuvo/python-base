93c9f0371efb5c6330360c2c577340c49e0f0cfa
thedenuvo <chelikov.n@yandex.ru>
2021-01-18 16:12:45 +0300

    Решил задачу 4

---
+++ lesson1/task4.py
@@ -0,0 +1,11 @@
+n = abs(int(input()))
+max = n % 10
+while n >= 1:
+    n = n // 10
+    if n % 10 > max:
+        max = n % 10
+    if n > 9:
+        continue
+    else:
+        print(max)
+        break
