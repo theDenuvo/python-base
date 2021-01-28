a9a4b8533fa1b08ffd0dc74ac90d4c97392d90da
thedenuvo <chelikov.n@yandex.ru>
2021-01-28 16:09:45 +0300

    Решил задачу 5

---
+++ lesson3/task5.py
@@ -0,0 +1,19 @@
+def my_sum ():
+    sum_res = 0
+    ex = False
+    while ex == False:
+        number = input('/ = exit').split()
+
+        res = 0
+        for el in range(len(number)):
+            if number[el] == '/' or number[el] == '/':
+                ex = True
+                break
+            else:
+                res = res + int(number[el])
+        sum_res = sum_res + res
+        print(sum_res)
+    print(f'final sum: {sum_res}')
+
+
+my_sum() 
