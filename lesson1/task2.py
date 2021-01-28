69ebf24d5dbc67b095c5e3a1c6b15184a2d71b44
theDenuvo <77621883+theDenuvo@users.noreply.github.com>
2021-01-28 19:16:13 +0300

    Delete lesson1 directory

--- lesson1/task2.py
+++
@@ -1,5 +0,0 @@
-time = int(input())
-hours = time // 3600
-minutes = (time - hours * 3600) // 60
-seconds = time - (hours * 3600 + minutes * 60)
-print(f"{hours} : {minutes} : {seconds}")
--- lesson1/task3.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
-print("Сумма чисел n + nn + nnn - %d" % total)
