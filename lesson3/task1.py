e5bf2d81506e47452ba94a579e26bb196abaaec5
theDenuvo <77621883+theDenuvo@users.noreply.github.com>
2021-01-28 14:46:57 +0300

    Update task1.py

--- lesson3/task1.py
+++ lesson3/task1.py
@@ -1,6 +1,16 @@
-a = int(input('Введите делимое: '))
-b = int(input("Ведите делитель: "))
-if a == 0 or b == 0:
-    print('error')
-else:
-    print(a / b)
+def check(c):
+    if c == 0:
+        print('error')
+    else:
+        return c
+
+
+def division(a, b):
+    check(a)
+    check(b)
+    print(a/b)
+
+
+num1 = int(input())
+num2 = int(input())
+division(num1, num2)
